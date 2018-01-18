9bade0cc0d34f99114df826a69b982562841b78d
Nickedude <niklas.erik.gustafson@gmail.com>
2018-01-18 15:55:06 +0100

    Notes and preparations for the ANN part

---
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -0,0 +1,17 @@
+# Artificial Neural Network
+
+# Installing Theano
+# Open source library for computations. 
+# Doesn't only run on the CPU, but on the GPU as well.
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Also a library for computations. Can run on a CPU or GPU. 
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# Used for building deep learning models with few lines of code.
+# "Wraps" Theano and Tensorflow.
+# pip install --upgrade keras
+
+# Part 1 - Data Preprocessing
